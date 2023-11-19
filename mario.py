9125ce051f03e04ec8c0bd6f497b32218bf172ff
bonillamiguel <150272397+bonillamiguel@users.noreply.github.com>
2023-11-19 17:27:13 -0500

    first commit

---
+++ mario.py
@@ -0,0 +1,19 @@
+def main():
+    print_square(3)
+
+
+def print_square(size):
+
+    # For each row in square
+    for i in range(size):
+
+        # For each brink in row
+        for j in range(size):
+
+            # Print brick
+            print("#", end="")
+            
+        print()
+
+
+main()
---
+++ sec2project.py
@@ -0,0 +1,33 @@
+# Write a Python function to calculate the area of a rectangle given its length and width.
+def main():
+    length = get_length()
+    width = get_width()
+    area = get_area(length, width)
+    print("The area of the rectangle is", area)
+
+def get_length():
+    while True:
+        try:
+            l = int(input("What's the length of the rectangle? "))
+            if l > 0:
+                return l
+            else:
+                print("Length must be a positive integer ")
+        except ValueError:
+            print("That is not an integer")
+
+def get_width():
+    while True:
+        try:
+            w = int(input("What's the width of the rectangle? "))
+            if w > 0:
+                return w
+            else:
+                print("Width must be a positive integer")
+        except ValueError:
+            print("That is not an integer")
+
+def get_area(length, width):
+    return length * width
+
+main()
